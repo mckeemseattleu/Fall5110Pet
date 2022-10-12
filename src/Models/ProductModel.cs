5a8cfa7c0c679d4ed6219065bc744a8d77641c69
mckeemseattleu <mckeem@seattleu.edu>
2022-10-12 14:13:31 -0700

    Refactored ProductModel.cs to be in alignment with Crudi version of Contoso Craft

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,7 +1,5 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -12,25 +10,10 @@ namespace ContosoCrafts.WebSite.Models
 
         [JsonPropertyName("img")]
         public string Image { get; set; }
-
         public string Url { get; set; }
-        
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public int[] Ratings { get; set; }
-
-        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
-
-        public string Quantity { get; set; }
-
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-        public int Price { get; set; }
-
-        // Store the Comments entered by the users on this product
-        public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
